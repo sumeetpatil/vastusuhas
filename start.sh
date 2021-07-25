b969202e26bfc25598379ad7b116cd33a91b3cf7
Sumeet PATIL <sumeet.patil@sap.com>
2021-07-25 13:48:55 +0200

    exits

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 nohup node index.js &
-exit 0
 echo "Service Started"
+exit 0
--- stop.sh
+++ stop.sh
@@ -2,3 +2,4 @@
 
 lsof -i :5000 | grep node | awk '{print $2}' | xargs kill -9
 echo "Service Stopped"
+exit 0
