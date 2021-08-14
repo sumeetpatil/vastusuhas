dc7f5d6a0b90ef17250eedfb75cb6c06b780072f
Sumeet PATIL <sumeet.patil@sap.com>
2021-08-14 20:26:36 +0200

    Calculate API

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var vastuCal = require('./public/js/vastuCal');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.get('/calculate', function(req, res) {
-    res.send('hello world');
+    let length = req.query.length;
+    let width = req.query.width;
+    res.send(vastuSuhasCal.calculate(length,width));
   });
 
 var server = app.listen(5000);
